645e078cb7bc42e1547dac131b214fe467bbaacd
mirko.fia <mirko.fia@innovaway.it>
2026-05-11 16:40:53 +0200

    Fix: Config caricato correttamente da file esterno

--- src/main/java/util/Config.java
+++ src/main/java/util/Config.java
@@ -1,5 +1,6 @@
 package util;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.util.Properties;
 
@@ -8,12 +9,26 @@ public class Config {
     private static Properties props = new Properties();
 
     static {
-        // Usiamo FileInputStream per leggere il file NELLA CARTELLA del programma
-        try (FileInputStream fis = new FileInputStream("config.properties")) {
-            props.load(fis);
-        } catch (Exception e) {
-            System.err.println("ERRORE: File config.properties non trovato nella cartella principale!");
-            e.printStackTrace();
+        // Usiamo il nuovo nome file che hai indicato
+        String fileName = "credenziali_database.properties";
+        
+        // Puntiamo alla cartella dove il programma viene lanciato
+        File configFile = new File(fileName);
+
+        // Questo log ti apparirà nel terminale e risolverà ogni dubbio
+        System.out.println("DEBUG: Cerco il file in: " + configFile.getAbsolutePath());
+
+        if (configFile.exists()) {
+            try (FileInputStream fis = new FileInputStream(configFile)) {
+                props.load(fis);
+                System.out.println("DEBUG: Caricamento riuscito!");
+            } catch (Exception e) {
+                System.err.println("ERRORE: Impossibile leggere il file trovato.");
+                e.printStackTrace();
+            }
+        } else {
+            System.err.println("ERRORE: Il file '" + fileName + "' non è stato trovato!");
+            System.err.println("Assicurati che sia nella stessa cartella del JAR.");
         }
     }
 
